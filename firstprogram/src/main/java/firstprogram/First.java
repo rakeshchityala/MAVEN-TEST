6047b92a83af3c0c1475c2c10ba08d10abbd2e3a
rakes <rakes@LAPTOP-DJ419Q3K>
2020-08-23 11:36:24 +0530

    First Commit

---
+++ firstprogram/src/main/java/firstprogram/First.java
@@ -0,0 +1,11 @@
+package firstprogram;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My frist program");
+
+	}
+
+}
---
+++ firstprogram/src/test/java/firstprogramtest/testfirst.java
@@ -0,0 +1,10 @@
+package firstprogramtest;
+
+public class testfirst {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My frist program test");
+
+	}
+
+}
