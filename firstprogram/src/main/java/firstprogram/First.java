705d1b47cc19505828e4e74550b1d603c547c63f
rakes <rakes@LAPTOP-DJ419Q3K>
2020-08-23 11:42:47 +0530

    second commit

--- firstprogram/src/main/java/firstprogram/First.java
+++ firstprogram/src/main/java/firstprogram/First.java
@@ -5,6 +5,7 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My frist program");
+		System.out.println("this is second line");
 
 	}
 
--- firstprogram/src/test/java/firstprogramtest/testfirst.java
+++ firstprogram/src/test/java/firstprogramtest/testfirst.java
@@ -4,6 +4,7 @@ public class testfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My frist program test");
+		System.out.println("This is second line in test");
 
 	}
 
